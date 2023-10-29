c4bdc5a208368eee0e255ef6d67f55c0a8d60ffc
emmanuel540! <emmanuelhillary24@gmail.com>
2023-10-29 14:44:24 +0300

    Done and dusted

--- server/app.py
+++ server/app.py
@@ -18,30 +18,39 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
 
+@app.before_request
+def check_if_logged_in():
+    if not session.get('user_id') and request.endpoint in [
+        'member_index',
+        'member_article',
+    ]:
+        return {'error': 'Unauthorized access'}, 401
+
+
+class ClearSession(Resource):
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
 
         return {}, 204
 
+
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return make_response(jsonify(articles), 200)
 
+
 class ShowArticle(Resource):
-
     def get(self, id):
-
         article = Article.query.filter(Article.id == id).first()
         article_json = article.to_dict()
 
         if not session.get('user_id'):
-            session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
+            session['page_views'] = (
+                0 if not session.get('page_views') else session.get('page_views')
+            )
             session['page_views'] += 1
 
             if session['page_views'] <= 3:
@@ -51,48 +60,51 @@ class ShowArticle(Resource):
 
         return article_json, 200
 
-class Login(Resource):
 
+class Login(Resource):
     def post(self):
-        
         username = request.get_json().get('username')
         user = User.query.filter(User.username == username).first()
 
         if user:
-        
             session['user_id'] = user.id
             return user.to_dict(), 200
 
         return {}, 401
 
+
 class Logout(Resource):
-
     def delete(self):
-
         session['user_id'] = None
-        
+
         return {}, 204
 
-class CheckSession(Resource):
 
+class CheckSession(Resource):
     def get(self):
-        
         user_id = session['user_id']
         if user_id:
             user = User.query.filter(User.id == user_id).first()
             return user.to_dict(), 200
-        
+
         return {}, 401
 
+
 class MemberOnlyIndex(Resource):
-    
     def get(self):
-        pass
+        articles = [article for article in Article.query.all()]
+        members_only_articles = []
+        for article in articles:
+            if article.is_member_only == True:
+                members_only_articles.append(article.to_dict())
+        return make_response(members_only_articles, 200)
+
 
 class MemberOnlyArticle(Resource):
-    
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id).first()
+        return article.to_dict(), 200
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
@@ -101,7 +113,9 @@ api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(MemberOnlyIndex, '/members_only_articles', endpoint='member_index')
-api.add_resource(MemberOnlyArticle, '/members_only_articles/<int:id>', endpoint='member_article')
+api.add_resource(
+    MemberOnlyArticle, '/members_only_articles/<int:id>', endpoint='member_article'
+)
 
 
 if __name__ == '__main__':
